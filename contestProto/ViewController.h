b819d6db4e23f05f9d94d2226f315894ddb3f15f
Kunal Desai <kunaljaydesai@gmail.com>
2014-10-09 18:25:10 -0700

    Initial Commit

---
+++ contestProto/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  contestProto
+//
+//  Created by Kunal Desai on 10/9/14.
+//  Copyright (c) 2014 myapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ contestProto/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  contestProto
+//
+//  Created by Kunal Desai on 10/9/14.
+//  Copyright (c) 2014 myapps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
